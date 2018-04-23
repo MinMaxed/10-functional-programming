598536c8bb9445f7a4e75ee4a1cb0ca354f3b4a1
Max <gusstorm132@gmail.com>
2018-04-23 10:52:10 -0700

    initPage loads, working on .map()s & handlebar template for admin

--- max-khaliil/public/scripts/article.js
+++ max-khaliil/public/scripts/article.js
@@ -1,7 +1,7 @@
 'use strict';
 var app = app || {};
 
-function articleIIFE(module) {
+(function (module) {
   function Article(rawDataObj) {
     // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
@@ -25,6 +25,7 @@ function articleIIFE(module) {
     /* OLD forEach():
     articleData.forEach(articleObject => Article.all.push(new Article(articleObject)));
     */
+    Article.all = articleData.map(articleObject => (new Article(articleObject)));
 
   };
 
@@ -35,13 +36,14 @@ function articleIIFE(module) {
         callback();
       })
   };
+  const reducer = (acc, currentValue) => acc + currentValue;
 
   Article.numWordsAll = () => {
-    return Article.all.map().reduce()
+    return Article.all.map(articleObject => articleObject.body.split(' ').length).reduce(reducer)
   };
 
   Article.allAuthors = () => {
-    return Article.all.map().reduce();
+    return Article.all.map(articleObject => articleObject.author).reduce( );
   };
 
   Article.numWordsByAuthor = () => {
@@ -91,4 +93,5 @@ function articleIIFE(module) {
       .then(console.log)
       .then(callback);
   };
-} articleIIFE();
+  module.Article = Article;
+}) (app);
